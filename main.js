ed4bb3f31ae7dae535fc3162ceed18c6a9dc9aa3
Connor Clark <cjamcl@gmail.com>
2019-03-28 23:40:43 -0700

    tsc, debug print

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ const traceLog = JSON.parse(fs.readFileSync('trace.json', 'utf-8'));
 
 const traceEventTypes = new Map();
 
+/**
+ * @param {*} event 
+ */
 function getTypeId(event) {
     return event.name.replace(/\s/g, '') + '_' + event.ph;
 }
 
 for (const event of traceLog.traceEvents) {
     if (event.name !== 'EvaluateScript') continue;
+    if (!event.args.data || !event.args.data.stackTrace) continue;
     const typeId = getTypeId(event);
     if (traceEventTypes.has(typeId)) continue;
 
@@ -17,41 +21,60 @@ for (const event of traceLog.traceEvents) {
     // console.log(typeId, event);
 }
 
+/** @typedef {{type: 'string' | 'number' | 'boolean' | TypeDef | TypeInfo, array?: boolean}} TypeDef */
+/** @typedef {Record<string, TypeDef>} TypeInfo */
+/** @typedef {{typeId: string, parent?: Type, info: TypeInfo}} Type */
+
+/**
+ * @param {*} object
+ */
 function getTypeInfo(object) {
+    /** @type {TypeInfo} */
     const info = {};
     for (const [key, value] of Object.entries(object)) {
-        if (typeof value === 'object') {
-            info[key] = getTypeInfo(value);
+        if (Array.isArray(value)) {
+            info[key] = {
+                type: getTypeInfo(value[0]), // maybe good enough.
+                array: true,
+            };
+        } else if (typeof value === 'object') {
+            info[key] = { type: getTypeInfo(value) };
         } else if (typeof value === 'string') {
-            info[key] = 'string';
+            info[key] = { type: 'string' };
         } else if (typeof value === 'number') {
-            info[key] = 'number';
+            info[key] = { type: 'number' };
         } else if (typeof value === 'boolean') {
-            info[key] = 'boolean';
+            info[key] = { type: 'boolean' };
         } else {
             throw new Error('what was that? ' + value);
         }
     }
 
+    /** @type {TypeInfo} */
     const ordered = {};
-    Object.keys(info).sort().forEach(function(key) {
+    Object.keys(info).sort().forEach(function (key) {
         ordered[key] = info[key];
     });
 
     return ordered;
 }
 
-// assume no object properties
+/**
+ * Assume no object properties
+ * @param {TypeInfo[]} typeInfos
+ * @return {Type}
+ */
 function findCommonType(typeInfos) {
+    /** @type {TypeInfo} */
     const commonInfo = {};
     const processed = new Set();
 
     for (const info of typeInfos) {
-        for (const [key, value] of Object.entries(info)) {
-            if (processed.has(key) || typeof value === 'object') continue;
+        for (const [key, typeDef] of Object.entries(info)) {
+            if (processed.has(key) || typeof typeDef.type === 'object') continue;
             processed.add(key);
 
-            if (typeInfos.every(info => key in info)) commonInfo[key] = value;
+            if (typeInfos.every(info => key in info)) commonInfo[key] = typeDef;
         }
     }
 
@@ -61,6 +84,7 @@ function findCommonType(typeInfos) {
     };
 }
 
+/** @type {Type[]} */
 const types = [];
 for (const [typeId, sampleEvent] of traceEventTypes.entries()) {
     const info = getTypeInfo(sampleEvent);
@@ -68,7 +92,7 @@ for (const [typeId, sampleEvent] of traceEventTypes.entries()) {
         typeId,
         info,
     });
-    console.log(typeId, info);
+    console.log(typeId, JSON.stringify(info, null, 2));
 
     // console.log(JSON.stringify(sampleEvent));
 }
@@ -78,59 +102,64 @@ const baseType = findCommonType(types.map(t => t.info));
 
 for (const type of types) {
     // TODO remove base props
-    type.parent = 'Base';
+    type.parent = baseType;
 }
 
 console.log('baseType', baseType);
 
+/**
+ * @param {Type[]} types 
+ */
 function print(types) {
+    const debugPrint = process.env.DEBUG_PRINT === '1';
     let output = '';
     let indentation = 0;
 
-    // function normalize(code) {
-    //     // const lines = code.split('\n');
-    //     // const minLeadingWs = lines.reduce((acc, line) => {
-    //     //     return Math.min(acc, line.search(/\S|$/));
-    //     // }, Number.POSITIVE_INFINITY);
-    //     // return lines.map(line => line.substring(minLeadingWs)).join('\n');
-    //     const lines = code.split('\n');
-    //     return lines.map(line => line.trim()).join('\n');
-    // }
-
-    // function normalizeFirstLine(code) {
-    //     const lines = code.split('\n');
-    //     lines[0] = lines[0].trim();
-    //     return lines.join('\n');
-    // }
-
+    /**
+     * @param {string} code
+     */
     function indent(code) {
         return code.split('\n')
             .map(line => '  ' + line)
             .join('\n');
     }
 
-    function printProperty(key, type) {
-        console.log('printProperty', indentation, key, type);
-        if (type && typeof type === 'object') {
-            return indent(`${key}: ${printObject(type)};`);
+    /**
+     * @param {string} key
+     * @param {TypeDef} typeDef
+     */
+    function printProperty(key, typeDef) {
+        if (debugPrint) console.log('printProperty', indentation, key, typeDef);
+        if (typeDef.type && typeof typeDef.type === 'object') {
+            // @ts-ignore - it's a TypeInfo
+            return indent(`${key}: ${printObject(typeDef.type)};`);
         } else {
-            return indent(`${key}: ${type};`);
+            return indent(`${key}: ${typeDef.type};`);
         }
     }
 
-    function printObject(type) {
-        console.log('printObject', indentation, type);
+    /**
+     * @param {TypeInfo} info 
+     */
+    function printObject(info) {
+        if (debugPrint) console.log('printObject', indentation, info);
         return I(() => {
-            return `{\n${Object.entries(type).map(([key, type]) => {
+            return `{\n${Object.entries(info).map(([key, type]) => {
                 return printProperty(key, type);
             }).join('\n')}\n}`;
         });
     }
 
-    function printInterface({typeId, info, parent}) {
-        return `interface ${typeId}${parent ? ' extends ' + parent : ''} ${printObject(info)}`;
+    /**
+     * @param {Type} type 
+     */
+    function printInterface(type) {
+        return `interface ${type.typeId}${type.parent ? ' extends ' + type.parent.typeId : ''} ${printObject(type.info)}`;
     }
 
+    /**
+     * @param {Function} fn 
+     */
     function I(fn) {
         indentation += 1;
         const result = fn();
@@ -142,7 +171,7 @@ function print(types) {
         output += printInterface(type) + "\n";
     }
 
-    return (output);
+    return output;
 }
 
 const result = print([baseType, ...types]);
